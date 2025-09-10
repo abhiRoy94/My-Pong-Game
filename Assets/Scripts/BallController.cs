0a9832c808b45f45d4b0d8d6d0b54cfaceba70f5
Abhishek Roy <83554653+abhiRoy94@users.noreply.github.com>
2025-09-10 16:57:01 -0400

    Calculate Collision point for ball + player

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,13 +1,17 @@
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
     public Rigidbody2D _rb;
-    public float ballSpeed = 5.0f;
+    public BoxCollider2D _playerBoxCollider;
+    public float ballSpeed = 2.5f;
+
+    private Vector2 _colliderSize;
 
     // Temporarily set the ball's direction to go left
-    private Vector2 _direction = new Vector2(-1, 0);
+    private Vector2 _direction = new Vector2(-1, -1);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -34,7 +38,23 @@ public class BallController : MonoBehaviour
     {
         if (collision.collider.CompareTag("Player"))
         {
+            
             Debug.Log("Hit the player!");
+
+            _playerBoxCollider = collision.collider as BoxCollider2D;  
+            if ( _playerBoxCollider != null )
+            {
+                // Get the midpoint of the player 
+                Vector2 _playerMidpoint = _playerBoxCollider.bounds.center;
+
+                // Determine the collision point between the ball and the player
+                Vector2 _collisionPoint = collision.GetContact(0).point;
+
+                // Calculate the height difference between the collision and the player midpoint
+                float _heightDifference = _playerMidpoint.y - _collisionPoint.y;
+                Debug.Log("Height Difference between midpoint and collision: " + _heightDifference);
+            }
+
             _direction = new Vector2(-_direction.x, -_direction.y);
         }
     }
